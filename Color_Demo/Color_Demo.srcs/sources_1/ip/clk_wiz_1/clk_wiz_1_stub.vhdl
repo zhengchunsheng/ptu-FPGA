b8ea54f0c4ef2ca881051ebfcd79002f0cb6d8ba
zhengchunsheng <1435749926@qq.com>
2020-08-01 20:57:07 +0800

    FPGA项目

---
+++ Color_Demo/Color_Demo.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Wed Jul 29 10:23:10 2020
+-- Host        : LAPTOP-SAC5FN0B running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               D:/SEA-master/Examples/FPGA/4.Module-Interface/MIPI-Camera-Interface/Image_Demo/Image_Demo.srcs/sources_1/ip/clk_wiz_1/clk_wiz_1_stub.vhdl
+-- Design      : clk_wiz_1
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s15ftgb196-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_1 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_1;
+
+architecture stub of clk_wiz_1 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
